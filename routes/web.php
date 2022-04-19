c55b0a3ef6f0960cf43af3b358c65c3ea3c040d6
Milan <milan.djukic@vivifyideas.com>
2022-04-19 11:36:32 +0200

    Added some basic routing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,30 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Returning here csrf token needed for POST, PUT, PATCH and DELETE request
+Route::get('/token', function(){
+    return csrf_token();
+})->name('token');
+
+Route::post('post', function (Request $request) {
+    $param1 =  $request->input('param1');
+    $param2 =  $request->input('param2');
+
+    return $param1." ".$param2;
+})->name('post');
+
+Route::put('put/{id}', function (Request $request,$id) {
+    $param1 = $request->get('param1');
+
+    return "Updating resource with id = ".$id." with new value: ".$param1;
+})->name('put');
+
+Route::delete('delete/{id}', function($id){
+    return "Deleted resource with id: ".$id;
+})->name('delete');
+
+Route::patch('patch/{id}', function(Request $request,$id){
+    $param1 = $request->get("param1");
+    return "Patching param1 with value: ".$param1;
+})->name('patch');
